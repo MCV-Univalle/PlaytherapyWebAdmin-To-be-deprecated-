35a4fb40a0c679406725207f6203f49002717bef
Miguel Angel Lopez <miguel.angel.lopez@correounivalle.edu.co>
2016-11-15 20:41:51 +0000

    #178 Added a verbose name

--- applications/start/models.py
+++ applications/start/models.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
@@ -6,40 +8,40 @@ from applications.therapist.models import *
 
 
 class TherapySession(models.Model):
-    date = models.DateField()
-    objective = models.CharField(max_length=128)
-    description = models.CharField(max_length=1024)
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
-    therapist = models.ForeignKey(Therapist, on_delete=models.CASCADE)
+    date = models.DateField(verbose_name='Fecha')
+    objective = models.CharField(max_length=128, verbose_name='Objetivo')
+    description = models.CharField(max_length=1024, verbose_name='Descripcion')
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, verbose_name='Paciente')
+    therapist = models.ForeignKey(Therapist, on_delete=models.CASCADE, verbose_name='Therapeuta')
     
     def __unicode__(self):
         return self.patient.name + " - " + self.therapist.name + " - " + self.objective[:20]
 
     
 class Movement(models.Model):
-    name = models.CharField(max_length=64, unique=True)
+    name = models.CharField(max_length=64, unique=True, verbose_name='Nombre')
     
     def __unicode__(self):
-        return str(self.name)
+        return self.name
     
 
 class Minigame(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    description = models.CharField(max_length=1024)
-    movements = models.ManyToManyField(Movement)
+    name = models.CharField(max_length=128, unique=True, verbose_name='Nombre')
+    description = models.CharField(max_length=1024, verbose_name='Descripción')
+    movements = models.ManyToManyField(Movement, verbose_name='Movimientos')
     
     def __unicode__(self):
         return self.name
 
 
 class GameSession(models.Model):
-    date = models.DateField()
-    score = models.IntegerField()
-    repetitions = models.IntegerField()
-    time = models.IntegerField()
-    level = models.IntegerField()
-    therapy = models.ForeignKey(TherapySession, on_delete=models.CASCADE)
-    minigame = models.ForeignKey(Minigame, on_delete=models.CASCADE)
+    date = models.DateField(verbose_name='Fecha')
+    score = models.IntegerField(verbose_name='Puntaje')
+    repetitions = models.IntegerField(verbose_name='Repeticiones')
+    time = models.IntegerField(verbose_name='Tiempo')
+    level = models.IntegerField(verbose_name='Nivel')
+    therapy = models.ForeignKey(TherapySession, on_delete=models.CASCADE, verbose_name='Terapeuta')
+    minigame = models.ForeignKey(Minigame, on_delete=models.CASCADE, verbose_name='Minijuego')
     movements = models.ManyToManyField(Movement, through="Performance")
     
     def __unicode__(self):
@@ -47,12 +49,12 @@ class GameSession(models.Model):
     
     
 class Performance(models.Model):
-    movement = models.ForeignKey(Movement, on_delete=models.CASCADE)
-    game_session = models.ForeignKey(GameSession, on_delete=models.CASCADE)
-    angle = models.IntegerField()
+    movement = models.ForeignKey(Movement, on_delete=models.CASCADE, verbose_name='Movimiento')
+    game_session = models.ForeignKey(GameSession, on_delete=models.CASCADE, verbose_name='Sesión de juego')
+    angle = models.IntegerField(verbose_name='Angulo')
     
     def __unicode__(self):
-        return movement
+        return self.movement.name + " Angle: " + str(self.angle)
     
     
     
