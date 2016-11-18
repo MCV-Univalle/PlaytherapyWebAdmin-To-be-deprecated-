dacda3070058d352f4f9fb8de3330a2b172dc031
Miguel Angel Lopez <miguel.angel.lopez@correounivalle.edu.co>
2016-11-18 21:34:14 +0000

    #189, #190 views ready

--- applications/reports/urls.py
+++ applications/reports/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url('por_movimiento/(?P<patient_id>\d+)/', views.by_movement, name='por_movimiento'),
-    url('por_minijuego/(?P<patient_id>\d+)/', views.by_minigame, name='por_minijuego'),
+    url('por-movimiento/(?P<patient_id>\d+)/', views.by_movement, name='por_movimiento'),
+    url('por-minijuego/(?P<patient_id>\d+)/', views.by_minigame, name='por_minijuego'),
+    url('por-fim/(?P<patient_id>\d+)/', views.by_fim, name='por_fim'),
 ]
--- applications/reports/views.py
+++ applications/reports/views.py
@@ -5,6 +5,7 @@ import json
 from models import *
 from forms import *
 from applications.start.models import *
+from applications.FIM.models import *
 
 
 @login_required # Verifies that the user is authenticated
@@ -50,7 +51,7 @@ def by_minigame(request, patient_id):
                     # for m in ms:
                     #     if m not in movements:
                     #         movements.append(m)
-                    # incorrecto
+            # incorrecto
             for performance in performances:
                 if performance.movement not in movements:
                     movements.append(performance.movement)
@@ -58,3 +59,42 @@ def by_minigame(request, patient_id):
     print performances
     print movements
     return render(request, 'reports/by_minigame.html', {'form': form, 'performances': performances, 'movements': movements, 'minigame': selected_minigame})
+    
+    
+@login_required # Verifies that the user is authenticated
+def by_fim(request, patient_id):
+    # patient_id  = 1
+    fims = []
+    form = ByFimReportForm()
+    if request.method == 'POST':
+        form = ByFimReportForm(request.POST)
+        if form.is_valid():
+            date1 = form.cleaned_data['date1']
+            date2 = form.cleaned_data['date2']
+            fims = FunctionalIndependenceMeasure.objects.filter(patient_id=patient_id, date__range=(date1, date2))
+    
+    print fims
+    return render(request, 'reports/by_fim.html', {'form': form, 'fims': fims})
+    
+    
+@login_required # Verifies that the user is authenticated
+def by_level(request, patient_id):
+    # patient_id  = 1
+    selected_minigame = None
+    levels = []
+    form = ByLevelReportForm()
+    if request.method == 'POST':
+        form = ByLevelReportForm(request.POST)
+        if form.is_valid():
+            date1 = form.cleaned_data['date1']
+            date2 = form.cleaned_data['date2']
+            selected_minigame = form.cleaned_data['minigame']
+            gss = selected_minigame.gamesession_set.filter(date__range=(date1, date2))
+            for gs in gss:
+                if gs.therapy.patient.id_num == patient_id:
+                    if gs.level not in levels:
+                        levels.append(gs.level)
+    
+    print levels
+    return render(request, 'reports/by_level.html', {'form': form, 'levels': levels, 'selected_minigame': selected_minigame})
+    
