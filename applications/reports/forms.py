5f6da99f3131e3ec94d41e816bfba39d71049d8d
Miguel Angel Lopez <miguel.angel.lopez@correounivalle.edu.co>
2017-02-15 22:03:18 +0000

    TG-251 #ready-for-test

--- applications/reports/forms.py
+++ applications/reports/forms.py
@@ -3,14 +3,25 @@
 from django import forms
 from django_select2.forms import Select2MultipleWidget, Select2Widget
 from datetimewidget.widgets import DateWidget
+import datetime
 
 from applications.start.models import *
 
 
+def date1():
+    date1 = datetime.date(datetime.date.today().year, 1, 1)
+    return date1
+    
+def date2():
+    date2 = datetime.date.today()
+    return date2
+    
+    
+
 class ByMovementReportForm(forms.Form):
     movement = forms.ModelChoiceField(queryset=Movement.objects.all(), widget=Select2Widget)
-    date1 = forms.DateField(widget=DateWidget(usel10n=True, bootstrap_version=3))
-    date2 = forms.DateField(widget=DateWidget(usel10n=True, bootstrap_version=3))
+    date1 = forms.DateField(widget=DateWidget(usel10n=True, bootstrap_version=3), initial=date1)
+    date2 = forms.DateField(widget=DateWidget(usel10n=True, bootstrap_version=3), initial=date2)
     
     def __init__(self, *args, **kwargs):
         super(ByMovementReportForm, self).__init__(*args, **kwargs)
@@ -69,3 +80,7 @@ class ByLevelReportForm(forms.Form):
         self.fields['date1'].label = 'Desde'
         self.fields['date2'].widget.attrs.update({'placeholder':'Fecha 2', 'required':'required'})
         self.fields['date2'].label = 'Hasta'
+        
+        
+
+
