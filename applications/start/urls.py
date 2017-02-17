44416374a292dcb57899f35bff52e123e9c735a3
Miguel Angel Lopez <miguel.angel.lopez@correounivalle.edu.co>
2017-02-17 17:39:56 +0000

    TG-278 #ready-for-test

--- applications/start/urls.py
+++ applications/start/urls.py
@@ -4,11 +4,12 @@ import django.contrib.auth.views
 import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
     url(r'login$', views.custom_login, name='login'),
     # url(r'^login$', django.contrib.auth.views.login, {'template_name':'init/login.html',
                                                         #  'authentication_form':LoginForm,}, name='login'),
     url(r'^logout/$', django.contrib.auth.views.logout, {'next_page': views.index}, name='logout'),
     # url(r'signup$', views.signup),
     url(r'dashboard$', views.dashboard, name='dashboard'),
+    url(r'prueba$', views.prueba),
 ]
